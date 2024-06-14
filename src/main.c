2e1fdd163abe3e9c2c58b943993bb3ffc0a4e71b
eeay0 <eeay0@protonmail.com>
2024-06-14 08:57:39 +0300

    restart

--- src/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-double answer;
-
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
--- src/operations.c
+++
@@ -1,9 +0,0 @@
-#include "../include/operations.h"
-
-double add(double a, double b) { return a + b; }
-
-double subtract(double a, double b) { return a - b; }
-
-double multiply(double a, double b) { return a * b; }
-
-double divide(double a, double b) { return a / b; }
