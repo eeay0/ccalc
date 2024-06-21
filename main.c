5e06c0f9398124bb0d193de13cef0f68e85a07a9
eeay0 <eeay0@protonmail.com>
2024-06-21 20:13:42 +0300

    list and stack implementation

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -20,11 +21,18 @@ typedef struct {
 typedef struct {
     Element *data;
     int top;
-    size_t size;
+    int size;
     size_t capacity;
 } Stack;
 
+typedef struct {
+    Element *data;
+    size_t end;
+    size_t capacity;
+} List;
+
 Element *tokenise(char *str);
+// Stack
 void initStack(Stack *s);
 void freeStack(Stack *s);
 void push(Stack *s, Element data);
@@ -32,24 +40,17 @@ Element pop(Stack *s);
 Element peek(Stack *s);
 bool isEmpty(Stack *s);
 bool isFull(Stack *s);
+void printStack(Stack *s);
+// List
+void initList(List *l);
+void freeList(List *l);
+void appendList(List *l, Element e);
+void printList(List *l);
+
+List infixToPostfix(Element *t);
 
 int main(int argc, char *argv[]) {
-    char test[30] = "3+ 43.67 -(6/2.5) * 2.2";
-    Element *tokens = tokenise(test);
-    printf("%s\n", test);
-    if (tokens == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; tokens[i].type != END; i++) {
-        if (tokens[i].type == OPERAND) {
-            printf("Operand: %lf\n", tokens[i].value.num);
-        } else if (tokens[i].type == OPERATOR) {
-            printf("Operator: %c\n", tokens[i].value.op);
-        }
-    }
-
-    free(tokens);
+    printf("All tests passed!\n");
     return EXIT_SUCCESS;
 }
 
@@ -104,7 +105,7 @@ void initStack(Stack *s) {
     s->top = -1;
     s->size = 0;
 }
-void freeStack(Stack *s) { free(s); }
+void freeStack(Stack *s) { free(s->data); }
 void push(Stack *s, Element data) {
     if (s->size == s->capacity) {
         s->capacity *= 2;
@@ -117,10 +118,19 @@ void push(Stack *s, Element data) {
     }
     s->data[++s->top] = data;
     s->size++;
+    if (s->size == s->capacity) {
+        s->capacity *= 2;
+        Element *tmp = realloc(s->data, s->capacity * sizeof(Element));
+        if (tmp == NULL) {
+            fprintf(stderr, "Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        s->data = tmp;
+    }
     s->data[s->top + 1].type = END;
 }
-bool isEmpty(Stack *s);
-bool isFull(Stack *s);
+bool isEmpty(Stack *s) { return s->size == 0; }
+bool isFull(Stack *s) { return s->size == s->capacity; }
 Element pop(Stack *s) {
     if (isEmpty(s)) {
         fprintf(stderr, "Stack is empty: POP");
@@ -130,4 +140,63 @@ Element pop(Stack *s) {
     s->data[s->top--].type = END;
     return tmp;
 }
-Element peek(Stack *s) {}
+Element peek(Stack *s) {
+    if (s->size == 0) {
+        fprintf(stderr, "Stack is empty: peek");
+        exit(EXIT_FAILURE);
+    }
+    return s->data[s->top];
+}
+
+void printStack(Stack *s) {
+    if (s->size == 0) {
+        fprintf(stderr, "Stack is empty: printStack");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; s->data->type != END; i++) {
+        if (s->data->type == OPERAND) {
+            printf("Operand: %lf\n", s->data->value.num);
+        } else if (s->data->type == OPERATOR) {
+            printf("Operator: %c\n", s->data->value.op);
+        }
+    }
+}
+
+void initList(List *l) {
+    l->end = -1;
+    l->capacity = INITIAL_CAPACITY;
+    l->data = malloc(l->capacity * sizeof(Element));
+    if (l->data == NULL) {
+        fprintf(stderr, "Failed to assign memory: initList");
+        exit(EXIT_FAILURE);
+    }
+}
+void freeList(List *l) { free(l->data); }
+void appendList(List *l, Element e) {
+    if (l->capacity == l->end + 1) {
+        l->capacity *= 2;
+        List *temp;
+        temp->data = realloc(l->data, l->capacity * sizeof(Element));
+        l = temp;
+    }
+    l->data[++l->end] = e;
+    l->capacity++;
+}
+void printList(List *l) {
+    if (l->end == -1) {
+        fprintf(stderr, "List is empty: printList");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; l->data->type != END; i++) {
+        if (l->data->type == OPERAND) {
+            printf("Operand: %lf\n", l->data->value.num);
+        } else if (l->data->type == OPERATOR) {
+            printf("Operator: %c\n", l->data->value.op);
+        }
+    }
+}
+List infixToPostfix(Element *t) {
+    List postfix;
+    Stack s;
+    return postfix;
+}
